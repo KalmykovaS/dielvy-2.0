1372d62299963a8f406db5c261f95a9885965a07
Svetlana Kalmykova <79218609932@ya.ru>
2021-08-01 14:01:15 +0300

    first commit

---
+++ source/js/script.js
@@ -0,0 +1,40 @@
+"use strict"
+
+const modalOpenedClass = `modal--opened`
+function showPopup(popupSelector) {
+  const popup = document.querySelector(popupSelector)
+  popup.classList.add(modalOpenedClass)
+}
+
+function closePopup() {
+  document.querySelectorAll(`.${modalOpenedClass}`).forEach((element) => {
+    element.classList.remove(modalOpenedClass)
+  })
+}
+
+document.addEventListener(`keydown`, (evt) => {
+  if (evt.key === `Escape`) {
+    evt.preventDefault()
+    closePopup()
+    closeMenu()
+  }
+})
+
+const navMain = document.querySelector(`.main-nav`)
+function toggleMenu() {
+  navMain.classList.toggle(`main-nav--opened`)
+  navMain.classList.toggle(`main-nav--closed`)
+}
+
+function closeMenu() {
+  navMain.classList.remove(`main-nav--opened`)
+  navMain.classList.add(`main-nav--closed`)
+}
+
+const header = document.querySelector(`.header`)
+const headerWrapper = header.querySelector(`.header__wrapper`)
+
+window.addEventListener(`scroll`, changeHeaderBackground)
+window.addEventListener(`resize`, changeHeaderBackground)
+
+
