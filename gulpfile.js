3b8e83a4c998b1c2d434c5c71b263f399632ec0b
Svetlana Kalmykova <79218609932@ya.ru>
2021-08-12 17:59:35 +0300

    done desktop and mobile version

--- gulpfile.js
+++ gulpfile.js
@@ -1,37 +1,49 @@
-"use strict"
+'use strict';
 
-const gulp = require(`gulp`)
-const plumber = require(`gulp-plumber`)
-const sourcemap = require(`gulp-sourcemaps`)
-const sass = require(`gulp-sass`)(require(`sass`))
-const postcss = require(`gulp-postcss`)
-const autoprefixer = require(`autoprefixer`)
-const server = require(`browser-sync`).create()
+const gulp = require('gulp');
+const plumber = require('gulp-plumber');
+const sourcemap = require('gulp-sourcemaps');
+const sass = require('gulp-sass')(require('sass'));
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const server = require('browser-sync').create();
+const svgstore = require('gulp-svgstore');
+const rename = require("gulp-rename");
 
-gulp.task(`css`, function () {
-  return gulp.src(`source/sass/style.scss`)
+gulp.task('css', function () {
+  return gulp.src('source/sass/style.scss')
     .pipe(plumber())
     .pipe(sourcemap.init())
     .pipe(sass())
     .pipe(postcss([
       autoprefixer()
     ]))
-    .pipe(sourcemap.write(`.`))
-    .pipe(gulp.dest(`source/css`))
-    .pipe(server.stream())
-})
+    .pipe(sourcemap.write('.'))
+    .pipe(gulp.dest('source/css'))
+    .pipe(server.stream());
+});
 
-gulp.task(`server`, function () {
+gulp.task('server', function () {
   server.init({
-    server: `source/`,
+    server: 'source/',
     notify: false,
     open: true,
     cors: true,
     ui: false
-  })
+  });
 
-  gulp.watch(`source/sass/**/*.{scss,sass}`, gulp.series(`css`))
-  gulp.watch(`source/*.html`).on(`change`, server.reload)
-})
+  gulp.watch('source/sass/**/*.{scss,sass}', gulp.series('css'));
+  gulp.watch('source/*.html').on('change', server.reload);
+});
 
-gulp.task(`start`, gulp.series(`css`, `server`))
+gulp.task('sprite', function () {
+  return gulp.src('source/img/icon/icon-*.svg')
+    .pipe(svgstore({
+      inlineSvg: true,
+
+    }))
+    .pipe(rename('sprite.svg'))
+    .pipe(gulp.dest('source/img'));
+});
+
+gulp.task('start', gulp.series('css', 'server'));
